137ad1189c3bc7348cd5da095024d28d909f5b75
hong <shmin7777@naver.com>
2021-10-16 00:59:47 +0900

    민석홍 커밋

--- src/main/java/com/example/test/controller/WebController.java
+++ src/main/java/com/example/test/controller/WebController.java
@@ -1,9 +1,15 @@
 package com.example.test.controller;
 
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Slf4j
 @Controller
@@ -33,4 +39,16 @@ public class WebController {
 
     @GetMapping("detail")
     public String detail(){return "detail";}
+
+    @GetMapping("map")
+    public String map(){return "map";}
+
+    @GetMapping("report")
+    public String report(){return "report";}
+
+
+
+
+
+
 }
