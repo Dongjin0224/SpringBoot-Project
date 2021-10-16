bd73c62df5c02a24b310ec4caa4535dfd8a581a2
dong <alalal2234@gmail.com>
2021-10-16 20:10:42 +0900

    3차 커밋 (결제 페이지)

---
+++ src/main/java/com/example/test/controller/WebController.java
@@ -0,0 +1,43 @@
+package com.example.test.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("")
+public class WebController {
+
+    @GetMapping("index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("elements")
+    public String elements(){
+        return "elements";
+    }
+
+    @GetMapping("generic")
+    public String generic(){
+        return "generic";
+    }
+
+    @GetMapping("mainBoard")
+    public String mainBoard(){ return "mainBoard"; }
+
+    @GetMapping("copy")
+    public String copy(){return "copy";}
+
+    @GetMapping("appointment")
+    public String appointment(){return "appointment";}
+
+    @GetMapping("membership")
+    public String membership(){return "membership";}
+
+    @GetMapping("payTerm")
+    public String payTerm(){return "payTerm";}
+
+}
