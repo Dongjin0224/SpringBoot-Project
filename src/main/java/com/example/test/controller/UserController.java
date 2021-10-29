aae7d0ae9fe0af6b7aaad36597bf011a051d1bb6
vanny <vannykim21@hotmail.com>
2021-10-29 15:02:42 +0900

    김이영 커밋(noticeBoard.html, UserController.java)

--- src/main/java/com/example/test/controller/UserController.java
+++ src/main/java/com/example/test/controller/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -25,6 +30,30 @@ public class UserController {
         log.info("---------------들어옴------------------");
         service.memberSignUp(vo);
         return "user/login";
+    }
+
+    /*@PostMapping("userLogin")
+    public RedirectView userLogin(UserVO vo, HttpServletRequest req, RedirectAttributes rttr) {
+        HttpSession session = req.getSession();
+        UserVO login = service.userLogin(vo);
+
+        if (login == null) {
+            session.setAttribute("user", null);
+            *//*rttr.addFlashAttribute("msg",false);*//*
+        } else {
+            session.setAttribute("user", login);
+        }
+        return new RedirectView("/index");
+    }*/
+
+
+    @GetMapping("userLogout")
+    public RedirectView userLogout(HttpServletRequest req) {
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        } return new RedirectView("redirect:/");
     }
 
 }
