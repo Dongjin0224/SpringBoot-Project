5bd57c843a8677888b8be372542a1bf505666531
vanny <vannykim21@hotmail.com>
2021-10-29 10:48:18 +0900

    김이영 커밋(MyPageController.java)

--- src/main/java/com/example/test/controller/MyPageController.java
+++ src/main/java/com/example/test/controller/MyPageController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyPageController {
     //  lim
     @GetMapping("user")
-    public String myPageUser(){return "myPage/user";}
+    public String myPageUser(){return "myPage/myPageUser";}
     //  lim
     @GetMapping("doc")
-    public String myPageDoc(){return "myPage/doc";}
+    public String myPageDoc(){return "myPage/myPageDoc";}
 }
