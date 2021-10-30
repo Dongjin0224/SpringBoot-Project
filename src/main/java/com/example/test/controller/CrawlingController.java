245c0c95b09d193ad16e351e8e2168945fba7a3c
lim <ljh3745043@naver.com>
2021-10-30 13:54:15 +0900

    10월 30일 14시 00분 크롤링 80%완료

---
+++ src/main/java/com/example/test/controller/CrawlingController.java
@@ -0,0 +1,112 @@
+package com.example.test.controller;
+
+
+import com.example.test.model.appointment.vo.ReserveVO;
+import com.example.test.services.AppointmentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequestMapping("/crawling/*")
+
+public class CrawlingController {
+
+    //WebDriver 설정
+    private WebDriver driver;
+    private WebElement element;
+    private String url;
+
+    //Properties 설정
+    public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    public static String WEB_DRIVER_PATH = "C:/chromedriver.exe";
+    public static String URL = "https://center-pf.kakao.com/_rxigIb/messages/new/feed";
+    // (여기선 naver 를 사용해봤습니다.)
+
+    public CrawlingController(){
+        //System Property SetUp
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        //Driver SetUp
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("ignoreProtectedModeSettings", true);
+        driver = new ChromeDriver(options);
+    }
+
+    public void open(){
+
+        try {
+            log.info("크롤링 시작");
+
+            driver.get(URL);
+            log.info("사이트 들어감");
+
+            element = driver.findElement(By.id("id_email_2"));
+            Thread.sleep(1000);
+            element.sendKeys("ljh3745043@naver.com");
+            log.info("아이디 입력 완료");
+
+            element = driver.findElement(By.id("id_password_3"));
+            Thread.sleep(1000);
+            element.sendKeys("jh5043");
+            log.info("비밀번호 입력 완료");
+
+
+            element = driver.findElement(By.className("submit"));
+            element.click();
+            log.info("로그인 버튼 클릭 완료");
+            Thread.sleep(1000);
+
+
+            String msg = "크롤링 실험 문자";   /* 보낼 문자 내용 설정 */
+            element = driver.findElement(By.cssSelector("textarea[name='messageWrite']"));
+            Thread.sleep(1000);
+            element.sendKeys(msg);
+            log.info("메세지 내용 입력 완료");
+
+            Thread.sleep(1000);
+            element = driver.findElement(By.className("btn_g2"));
+            element.click();
+            log.info("다음버튼 클릭 완료");
+            Thread.sleep(1000);
+
+            WebElement parent = driver.findElement(By.className("wrap_btn"));
+            List<WebElement> button = parent.findElements(By.className("btn_g"));
+            button.get(2).click();
+            log.info("테스트 발송 버튼 클릭 완료");
+            Thread.sleep(1000);
+
+            WebElement input = null;
+            input = driver.findElement(By.id("phoneNumber"));
+            input.sendKeys("01091685043");  /* 진료 예약한 회원 번호와, 의사 번호 입력 */
+            Thread.sleep(1000);
+            log.info("핸드폰번호 입력 완료");
+
+            Thread.sleep(1000);
+            WebElement parent2 = driver.findElement(By.className("certify_g"));
+            List<WebElement> button2 = parent2.findElements(By.className("btn_g2"));
+            button2.get(0).click();
+            log.info("전송 완료");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            driver.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        CrawlingController test = new CrawlingController();
+        test.open();
+    }
+
+}
--- src/main/java/com/example/test/model/appointment/dao/AppointmentDAO.java
+++ src/main/java/com/example/test/model/appointment/dao/AppointmentDAO.java
@@ -1,6 +1,9 @@
 package com.example.test.model.appointment.dao;
 
 import com.example.test.mappers.AppointmentMapper;
+import com.example.test.model.appointment.vo.ReserveDocVO;
+import com.example.test.model.appointment.vo.ReserveUserVO;
+import com.example.test.model.appointment.vo.ReserveVO;
 import com.example.test.model.user.vo.DocAttachFileVO;
 import com.example.test.model.user.vo.DocVO;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +19,7 @@ public class AppointmentDAO {
 
     public DocVO get(Long docNo){ return appointmentMapper.read(docNo); }
     public DocAttachFileVO getFile(Long docNo){ return appointmentMapper.getFile(docNo); }
-
+    public void reserve(ReserveVO reserveVO){ appointmentMapper.reserve(reserveVO); }
+    public ReserveUserVO getUserPhone(Long userNo){ return appointmentMapper.getUserPhone(userNo); }
+    public ReserveDocVO getDocPhone(Long docNo){ return appointmentMapper.getDocPhone(docNo); }
 }
