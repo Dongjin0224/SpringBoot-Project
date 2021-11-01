482fff5ccbbf255ea5f9a07a9491791faa4b4958
lim <ljh3745043@naver.com>
2021-11-01 13:28:05 +0900

    11월 01일 13시 30분 합치기 시도

--- src/main/java/com/example/test/services/DocServiceImple.java
+++ src/main/java/com/example/test/services/DocServiceImple.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class DocSeviceImple implements DocService{
+public class DocServiceImple implements DocService{
 
     private final DocDAO docDAO;
     private final DocAttachFileDAO docAttachFileDAO;
